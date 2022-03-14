74d5c8f009947e8e16f2335e46bade8e02ca15d1
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2022-03-14 19:22:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,4 @@
-/*challange description:
- * Ask the user to enter a word
- * process the Romeo and Juliet file and determine how many total words are there
- * and how many times the word the user enterd apppears as a substring of s word in the play
- * for example:
- * if the user enters: love
- * Then the words love, lovely, and beloved will all be considered matches
- * you decide whether you want to be case sensative or not my solution is case sensative
- *
- * sample:
- * enter the substring to search for : love
- * 25919 words were searched
- * The substring love was found 171 times
-*/
+
 
 #include <iostream>
 #include<string>
